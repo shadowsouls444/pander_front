1c453c00c94b7a99e0faf01e1715374896853d0a
Johanbeltranramirez <johanframirez13@gmail.com>
2026-08-16 15:20:38 -0500

    d

--- src/routes/index.jsx
+++
@@ -1,7 +0,0 @@
-// src/routes/index.jsx
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routers'
-
-const AppRouter = () => <RouterProvider router={router} />
-
-export default AppRouter
